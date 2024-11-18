822ed246acbf335d57e56a1274d936e815e7eaf3
Liam <liamdewitt96@gmail.com>
2024-11-17 22:42:06 -0500

    feat: add prompt for player control

---
+++ src/utils/playerControls.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+
+const promptForPlayerControls = async () => {
+  const result = await inquirer.prompt({
+    type: "list",
+    name: "controls",
+    message: "Which path will you venture through?",
+    choices: ["Up", "Down", "Left", "Right"],
+  });
+  return result;
+};
+
+promptForPlayerControls();
+export { promptForPlayerControls };
