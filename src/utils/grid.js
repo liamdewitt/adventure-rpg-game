00aa63ced2c96fb016eed464fc447aa04186b288
Liam <liamdewitt96@gmail.com>
2024-11-18 21:16:42 -0500

    feat(grid): update class Grid properties
    - add grid displaying as strings
    - add game start method on instance
    - add player movement (WIP)

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -1,18 +1,53 @@
+import { promptForPlayersDirection } from "./playerPrompts.js";
+
 class Grid {
-  constructor(width, height) {
+  constructor(width, height, playerPosX = 0, playerPosY = height - 1) {
     this.width = width;
     this.height = height;
+    this.playerX = playerPosX;
+    this.playerY = playerPosY;
 
     this.grid = [];
     for (let row = 0; row < this.height; row++) {
       let thisRow = [];
       for (let col = 0; col < this.width; col++) {
-        thisRow.push("test");
+        thisRow.push("T");
       }
       this.grid.push(thisRow);
     }
+    //
+    this.grid[0][this.grid[0].length - 1] = "G";
+    this.grid[height - 1][0] = "P"; //this.grid[height - 1][0]
+    // console.log(this.grid);
+    this.gameStart();
+  }
+
+  async gameStart() {
+    while (true) {
+      this.displayGrid();
+      const result = await promptForPlayersDirection();
+      if (result === "Up") {
+        this.playerMoveUp();
+        console.log("---------------------");
+        // console.log(this.grid);
+      }
+    }
+  }
+
+  displayGrid() {
+    for (let rowIndex = 0; rowIndex < this.grid.length; rowIndex++) {
+      console.log(this.grid[rowIndex].join("        "));
+    }
+  }
+
+  playerMoveUp() {
+    if (this.grid[0][this.playerX] === "P") {
+      return console.log("You cannot move up");
+    }
+    this.grid[this.playerY][this.playerX] = "W";
+    this.grid[(this.playerY -= 1)][this.playerX] = "P";
   }
 }
 
-const grid = new Grid(5, 10);
-console.log(grid);
+const grid = new Grid(3, 5);
+// console.log(grid);
