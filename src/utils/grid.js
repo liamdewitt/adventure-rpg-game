334419cf5ca99d2b88125bdc4a93d772593d344c
Liam <liamdewitt96@gmail.com>
2024-11-20 23:56:08 -0500

    feat(grid): add event spawner - add what little emojis can actually be used natively

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -1,3 +1,6 @@
+import { EnemyObject } from "./enemyObject.js";
+import { GridObject } from "./gridObject.js";
+import { ItemObject } from "./itemObject.js";
 import { promptForPlayersDirection } from "./playerPrompts.js";
 
 class Grid {
@@ -11,19 +14,19 @@ class Grid {
     for (let row = 0; row < this.height; row++) {
       let thisRow = [];
       for (let col = 0; col < this.width; col++) {
-        thisRow.push("T");
+        thisRow.push("☘️");
       }
       this.grid.push(thisRow);
     }
     //
-    this.grid[0][this.grid[0].length - 1] = "G";
-    this.grid[height - 1][0] = "O"; //this.grid[height - 1][0]
+    this.grid[0][this.grid[0].length - 1] = "⭐";
+    this.grid[height - 1][0] = "♚"; //this.grid[height - 1][0]
     this.gameStart();
   }
 
   async gameStart() {
     while (true) {
-      if (this.grid[0][this.grid[0].length - 1] === "O") {
+      if (this.grid[0][this.grid[0].length - 1] === "♚") {
         console.log("Congratulations! You've reached the goal!");
         break;
       }
@@ -48,12 +51,41 @@ class Grid {
     }
   }
 
+  eventSpawner() {
+    if (this.grid[0][this.grid[0].length - 1] === "♚") {
+      return;
+    }
+    const randomNum = Math.floor(Math.random() * 10) + 1;
+    if (randomNum <= 3) {
+      const enemyEncounter = new EnemyObject({
+        name: "spider",
+        attack: 1,
+        defense: 2,
+        hp: 3,
+      });
+      enemyEncounter.describe();
+      return enemyEncounter;
+    } else if (randomNum > 4 && randomNum <= 5) {
+      const itemDiscovery = new ItemObject({
+        name: "sword",
+        attack: 2,
+        defense: 3,
+        hp: 4,
+      });
+      itemDiscovery.describe();
+      return itemDiscovery;
+    }
+    const noEncounter = new GridObject();
+    noEncounter.describe();
+  }
+
   playerMoveUp() {
     if (this.playerY === 0) {
       return console.log("You cannot move up");
     }
     this.grid[this.playerY][this.playerX] = "x";
-    this.grid[(this.playerY -= 1)][this.playerX] = "O";
+    this.grid[(this.playerY -= 1)][this.playerX] = "♚";
+    this.eventSpawner();
   }
 
   playerMoveDown() {
@@ -61,7 +93,8 @@ class Grid {
       return console.log("You cannot move down");
     }
     this.grid[this.playerY][this.playerX] = "x";
-    this.grid[(this.playerY += 1)][this.playerX] = "O";
+    this.grid[(this.playerY += 1)][this.playerX] = "♚";
+    this.eventSpawner();
   }
 
   playerMoveLeft() {
@@ -69,7 +102,8 @@ class Grid {
       return console.log("You cannot move left");
     }
     this.grid[this.playerY][this.playerX] = "x";
-    this.grid[this.playerY][(this.playerX -= 1)] = "O";
+    this.grid[this.playerY][(this.playerX -= 1)] = "♚";
+    this.eventSpawner();
   }
 
   playerMoveRight() {
@@ -77,7 +111,8 @@ class Grid {
       return console.log("You cannot move right");
     }
     this.grid[this.playerY][this.playerX] = "x";
-    this.grid[this.playerY][(this.playerX += 1)] = "O";
+    this.grid[this.playerY][(this.playerX += 1)] = "♚";
+    this.eventSpawner();
   }
 }
 
